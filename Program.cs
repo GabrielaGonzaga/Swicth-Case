f012a60364fe5ff5567a180e020f1729c78c81c7
GabrielaGonzaga <73176744+GabrielaGonzaga@users.noreply.github.com>
2020-11-16 20:56:40 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Swicth_Case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.WriteLine("Digite o tipo de operação");
+            string operaçao = Console.ReadLine();
+            
+            Console.WriteLine("Digite o 1° número");
+            float num1 =float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Digite o 2° número");
+            float num2 =float.Parse(Console.ReadLine());
+
+            float resultado =0;
+
+           switch(operaçao){
+            
+            case "Soma":
+            resultado = num1+num2;
+            break;
+            
+            case "Subtração":
+            resultado = num1-num2;
+            break;
+            
+            case "Multiplicação":
+            resultado = num1*num2;
+            break;
+            
+            case "Divisão":
+            resultado = num1/num2;
+            break;
+
+            default:
+            Console.WriteLine("Operação inválida");
+            break;
+            }
+
+            
+
+        }
+    }
+}
